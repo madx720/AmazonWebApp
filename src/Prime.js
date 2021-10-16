f158f3cbf19a72000aa4fd145805c058c90b9312
karan <karan.dev066@gmail.com>
2021-10-16 16:43:26 +0530

    Added source code

---
+++ src/Prime.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Prime.css";
+
+function Prime() {
+  return (
+    <div className="prime">
+      <h2>This is Prime Page</h2>
+    </div>
+  );
+}
+
+export default Prime;
